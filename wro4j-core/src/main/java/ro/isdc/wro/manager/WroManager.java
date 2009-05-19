3b710c59248cf663eea914cc6e82abdf2c0536dd
ovi2004 <ovi2004@14e8a440-08f4-11de-bc6d-132d5dcf21d4>
2009-05-19 13:03:19 +0000

    added dynamic params support
    git-svn-id: https://wro4j.googlecode.com/svn/trunk@41 14e8a440-08f4-11de-bc6d-132d5dcf21d4

--- wro4j-core/src/main/java/ro/isdc/wro/manager/WroManager.java
+++ wro4j-core/src/main/java/ro/isdc/wro/manager/WroManager.java
@@ -4,7 +4,12 @@
 package ro.isdc.wro.manager;
 
 import java.io.ByteArrayInputStream;
+import java.io.IOException;
 import java.util.List;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.apache.commons.lang.time.StopWatch;
 import org.apache.commons.logging.Log;
@@ -12,6 +17,7 @@ import org.apache.commons.logging.LogFactory;
 
 import ro.isdc.wro.cache.CacheStrategy;
 import ro.isdc.wro.exception.WroRuntimeException;
+import ro.isdc.wro.http.ContextHolder;
 import ro.isdc.wro.model.Group;
 import ro.isdc.wro.model.WroModel;
 import ro.isdc.wro.model.WroModelFactory;
@@ -21,9 +27,8 @@ import ro.isdc.wro.resource.ResourceType;
 import ro.isdc.wro.resource.UriLocatorFactory;
 
 /**
- * WroManager. Contains all the factories used by optimizer in order to perform
- * the logic.
- * 
+ * WroManager. Contains all the factories used by optimizer in order to perform the logic.
+ *
  * @author alexandru.objelean / ISDC! Romania
  * @version $Revision: $
  * @date $Date: $
@@ -34,6 +39,11 @@ public final class WroManager {
    * Logger for this class.
    */
   private static final Log log = LogFactory.getLog(WroManager.class);
+
+  /**
+   * Dynamic parameters regexp pattern
+   */
+  private static Pattern paramPattern = Pattern.compile("\\$\\{(.+?)\\}");
 
   /**
    * ResourcesModel factory.
@@ -56,21 +66,20 @@ public final class WroManager {
   private GroupsProcessor groupsProcessor;
 
   /**
-   * A cacheStrategy used for caching processed results. Key: uri, Value:
-   * processed result.
+   * A cacheStrategy used for caching processed results. Key: uri, Value: processed result.
    */
   private CacheStrategy<String, String> cacheStrategy;
 
   /**
    * Default constructor.
    */
-  public WroManager() {}
+  public WroManager() {
+  }
 
   /**
-   * Perform processing of the uri and depending on it will return
-   * {@link WroProcessResult} object containing the input stream of the
-   * requested resource and it's content type.
-   * 
+   * Perform processing of the uri and depending on it will return {@link WroProcessResult} object containing
+   * the input stream of the requested resource and it's content type.
+   *
    * @param uri
    * @return
    */
@@ -103,7 +112,11 @@ public final class WroManager {
 
     final WroProcessResult result = new WroProcessResult();
     result.setContentType(getContentType(type));
-    result.setInputStream(new ByteArrayInputStream(processedResult.getBytes()));
+
+    // replace expressions before writing to stream
+    String exprReplaced = replaceExpressions(processedResult);
+
+    result.setInputStream(new ByteArrayInputStream(exprReplaced.getBytes()));
 
     stopWatch.stop();
     log.info("WroManage process time: " + stopWatch.toString());
@@ -113,8 +126,59 @@ public final class WroManager {
   }
 
   /**
-   * @param type
-   *          {@link ResourceType} object.
+   * Try to replace the params expressions with suitable values.
+   *
+   * @param configStream
+   * @return
+   * @throws IOException
+   */
+  private String replaceExpressions(String resource) {
+    StringBuilder builder = new StringBuilder(resource);
+    Matcher matcher = null;
+
+    // process config
+    matcher = paramPattern.matcher(builder.toString());
+
+    // look up all the parameters
+    while (matcher.find()) {
+      String parameter = matcher.group(1);
+
+      // replace the param place holder with the value
+      String replaced = matcher.replaceFirst(acquireParamValue(parameter));
+      builder.replace(0, builder.length() - 1, replaced);
+      matcher = paramPattern.matcher(builder.toString());
+    }
+
+    return builder.toString();
+  }
+
+  /**
+   * Get the parameters from request or session.
+   *
+   * @param parameter
+   * @return
+   */
+  private String acquireParamValue(String parameter) {
+    HttpServletRequest request = ContextHolder.REQUEST_HOLDER.get();
+
+    // attempt to get the parameter from the request first
+    String paramValue = request.getParameter(parameter);
+
+    if (paramValue == null) {
+      // not a request param, now attempt to get it from session
+      paramValue = (String) request.getSession().getAttribute(parameter);
+    }
+
+    // as last resort will set the value as the parameter name
+    if (paramValue == null) {
+      paramValue = parameter;
+    }
+
+    return paramValue;
+  }
+
+  /**
+   * @param type {@link ResourceType} object.
    * @return content type depending on resourceType.
    */
   private String getContentType(final ResourceType type) {
@@ -153,44 +217,37 @@ public final class WroManager {
   }
 
   /**
-   * @param uriProcessor
-   *          the uriProcessor to set
+   * @param uriProcessor the uriProcessor to set
    */
   public final void setUriProcessor(final UriProcessor uriProcessor) {
     this.uriProcessor = uriProcessor;
   }
 
   /**
-   * @param groupsProcessor
-   *          the groupsProcessor to set
+   * @param groupsProcessor the groupsProcessor to set
    */
   public final void setGroupsProcessor(final GroupsProcessor groupsProcessor) {
     this.groupsProcessor = groupsProcessor;
   }
 
   /**
-   * @param modelFactory
-   *          the modelFactory to set
+   * @param modelFactory the modelFactory to set
    */
   public final void setModelFactory(final WroModelFactory modelFactory) {
     this.modelFactory = modelFactory;
   }
 
   /**
-   * @param uriLocatorFactory
-   *          the resourceLocatorFactory to set
+   * @param uriLocatorFactory the resourceLocatorFactory to set
    */
-  public final void setUriLocatorFactory(
-      final UriLocatorFactory uriLocatorFactory) {
+  public final void setUriLocatorFactory(final UriLocatorFactory uriLocatorFactory) {
     this.uriLocatorFactory = uriLocatorFactory;
   }
 
   /**
-   * @param cacheStrategy
-   *          the cache to set
+   * @param cacheStrategy the cache to set
    */
-  public final void setCacheStrategy(
-      final CacheStrategy<String, String> cacheStrategy) {
+  public final void setCacheStrategy(final CacheStrategy<String, String> cacheStrategy) {
     this.cacheStrategy = cacheStrategy;
   }
 
