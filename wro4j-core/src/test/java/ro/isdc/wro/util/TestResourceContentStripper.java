719e3d72b3b43d828a40245f07d3a39d8d62e2a1
alex.objelean <alex.objelean@14e8a440-08f4-11de-bc6d-132d5dcf21d4>
2009-08-02 17:23:46 +0000

    add test class
    git-svn-id: https://wro4j.googlecode.com/svn/trunk@560 14e8a440-08f4-11de-bc6d-132d5dcf21d4

---
+++ wro4j-core/src/test/java/ro/isdc/wro/util/TestResourceContentStripper.java
@@ -0,0 +1,12 @@
+/*
+ * Copyright (c) 2009 Wro4j. All rights reserved.
+ */
+package ro.isdc.wro.util;
+
+/**
+ * Test class for {@link ResourceContentStripper} class.
+ * @author Alex Objelean
+ */
+public class TestResourceContentStripper {
+
+}
