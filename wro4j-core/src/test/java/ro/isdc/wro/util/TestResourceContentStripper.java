e8e8f2a785ff9fe5cafc3daeabbaed1cb6c6b50d
alex.objelean <alex.objelean@14e8a440-08f4-11de-bc6d-132d5dcf21d4>
2009-11-13 13:15:58 +0000

    Create HttpHeader enum, server-side caching updates & other small improvements
    git-svn-id: https://wro4j.googlecode.com/svn/trunk@596 14e8a440-08f4-11de-bc6d-132d5dcf21d4

--- wro4j-core/src/test/java/ro/isdc/wro/util/TestResourceContentStripper.java
+++ wro4j-core/src/test/java/ro/isdc/wro/util/TestResourceContentStripper.java
@@ -3,10 +3,15 @@
  */
 package ro.isdc.wro.util;
 
+import org.junit.Test;
+
 /**
  * Test class for {@link ResourceContentStripper} class.
  * @author Alex Objelean
  */
 public class TestResourceContentStripper {
-
+	@Test
+	public void empty() {
+		//TODO implement
+	}
 }
