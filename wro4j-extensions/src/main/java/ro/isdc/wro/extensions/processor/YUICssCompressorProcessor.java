418c61711d1b320b1affb604ffabc536788f5e6a
alex.objelean <alex.objelean@14e8a440-08f4-11de-bc6d-132d5dcf21d4>
2009-11-16 22:40:37 +0000

    use logger instead of system.err
    git-svn-id: https://wro4j.googlecode.com/svn/trunk@598 14e8a440-08f4-11de-bc6d-132d5dcf21d4

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/YUICssCompressorProcessor.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/YUICssCompressorProcessor.java
@@ -12,38 +12,39 @@ import ro.isdc.wro.processor.ResourcePreProcessor;
 
 import com.yahoo.platform.yui.compressor.CssCompressor;
 
+
 /**
- * YUICssCompressorProcessor. Use YUI css compression utility for processing a
- * css resource.
- * 
+ * YUICssCompressorProcessor. Use YUI css compression utility for processing a css resource.
+ *
  * @author Alexandru.Objelean / ISDC! Romania
  * @version $Revision: $
  * @date $Date: $
  * @created Created on Dec 4, 2008
  */
-public class YUICssCompressorProcessor implements ResourcePreProcessor,
-    ResourcePostProcessor {
+public class YUICssCompressorProcessor
+  implements ResourcePreProcessor, ResourcePostProcessor {
   /**
    * An option of CssCompressor.
    */
   private static final int linebreakpos = -1;
 
+
   /**
    * {@inheritDoc}
    */
-  public void process(final String resourceUri, final Reader reader,
-      final Writer writer) throws IOException {
+  public void process(final String resourceUri, final Reader reader, final Writer writer)
+    throws IOException {
     // resourceUri doesn't matter
     this.process(reader, writer);
   }
+
 
   /**
    * {@inheritDoc}
    */
   public void process(final Reader reader, final Writer writer)
-      throws IOException {
+    throws IOException {
     final CssCompressor compressor = new CssCompressor(reader);
     compressor.compress(writer, linebreakpos);
   }
-
 }
--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/YUIJsCompressorProcessor.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/YUIJsCompressorProcessor.java
@@ -9,23 +9,30 @@ import java.io.Writer;
 
 import org.mozilla.javascript.ErrorReporter;
 import org.mozilla.javascript.EvaluatorException;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import ro.isdc.wro.processor.ResourcePostProcessor;
 import ro.isdc.wro.processor.ResourcePreProcessor;
 
 import com.yahoo.platform.yui.compressor.JavaScriptCompressor;
 
+
 /**
- * YUICssCompressorProcessor. Use YUI css compression utility for processing a
- * css resource.
- * 
+ * YUICssCompressorProcessor. Use YUI css compression utility for processing a css resource.
+ *
  * @author Alexandru.Objelean / ISDC! Romania
  * @version $Revision: $
  * @date $Date: $
  * @created Created on Dec 4, 2008
  */
-public class YUIJsCompressorProcessor implements ResourcePreProcessor,
-    ResourcePostProcessor {
+public class YUIJsCompressorProcessor
+  implements ResourcePreProcessor, ResourcePostProcessor {
+  /**
+   * Logger for this class.
+   */
+  private static final Logger log = LoggerFactory.getLogger(YUIJsCompressorProcessor.class);
+
   // options of YUI compressor
   private final int linebreakpos = -1;
 
@@ -37,49 +44,49 @@ public class YUIJsCompressorProcessor implements ResourcePreProcessor,
 
   boolean verbose = true;
 
+
   /**
    * {@inheritDoc}
    */
-  public void process(final String resourceUri, final Reader reader,
-      final Writer writer) throws IOException {
+  public void process(final String resourceUri, final Reader reader, final Writer writer)
+    throws IOException {
     // resourceUri doesn't matter
     this.process(reader, writer);
   }
+
 
   /**
    * {@inheritDoc}
    */
   public void process(final Reader reader, final Writer writer)
-      throws IOException {
-    final JavaScriptCompressor compressor = new JavaScriptCompressor(reader,
-        new ErrorReporter() {
-          public void warning(String message, String sourceName, int line,
-              String lineSource, int lineOffset) {
-            if (line < 0) {
-              System.err.println("\n[WARNING] " + message);
-            } else {
-              System.err.println("\n[WARNING] " + line + ':' + lineOffset + ':'
-                  + message);
-            }
-          }
+    throws IOException {
+    final JavaScriptCompressor compressor = new JavaScriptCompressor(reader, new ErrorReporter() {
+      public void warning(final String message, final String sourceName, final int line, final String lineSource,
+        final int lineOffset) {
+        if (line < 0) {
+          log.warn("\n[WARNING] " + message);
+        } else {
+          log.warn("\n[WARNING] " + line + ':' + lineOffset + ':' + message);
+        }
+      }
 
-          public void error(String message, String sourceName, int line,
-              String lineSource, int lineOffset) {
-            if (line < 0) {
-              System.err.println("\n[ERROR] " + message);
-            } else {
-              System.err.println("\n[ERROR] " + line + ':' + lineOffset + ':'
-                  + message);
-            }
-          }
 
-          public EvaluatorException runtimeError(String message,
-              String sourceName, int line, String lineSource, int lineOffset) {
-            error(message, sourceName, line, lineSource, lineOffset);
-            return new EvaluatorException(message);
-          }
-        });
-    compressor.compress(writer, linebreakpos, munge, verbose,
-        preserveAllSemiColons, disableOptimizations);
+      public void error(final String message, final String sourceName, final int line, final String lineSource,
+        final int lineOffset) {
+        if (line < 0) {
+          log.error("\n[ERROR] " + message);
+        } else {
+          log.error("\n[ERROR] " + line + ':' + lineOffset + ':' + message);
+        }
+      }
+
+
+      public EvaluatorException runtimeError(final String message, final String sourceName, final int line,
+        final String lineSource, final int lineOffset) {
+        error(message, sourceName, line, lineSource, lineOffset);
+        return new EvaluatorException(message);
+      }
+    });
+    compressor.compress(writer, linebreakpos, munge, verbose, preserveAllSemiColons, disableOptimizations);
   }
 }
