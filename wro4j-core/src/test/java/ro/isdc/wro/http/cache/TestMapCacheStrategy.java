6b470c8debf7f0f30b90559e2faf2e749cfcbf7a
Alex Objelean <alex.objelean@gmail.com>
2010-04-12 17:59:19 +0300

    update CacheEntry hashcode => fix exception when dealing with dynamic resources

---
+++ wro4j-core/src/test/java/ro/isdc/wro/http/cache/TestMapCacheStrategy.java
@@ -0,0 +1,36 @@
+/*
+ * Copyright (C) 2010.
+ * All rights reserved.
+ */
+package ro.isdc.wro.http.cache;
+
+import junit.framework.Assert;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import ro.isdc.wro.cache.CacheEntry;
+import ro.isdc.wro.cache.CacheStrategy;
+import ro.isdc.wro.cache.impl.MapCacheStrategy;
+import ro.isdc.wro.model.resource.ResourceType;
+
+/**
+ * Test class for MapCacheStrategy.
+ *
+ * @author Alex Objelean
+ */
+public class TestMapCacheStrategy {
+  private CacheStrategy<CacheEntry, String> strategy;
+  @Before
+  public void setUp() {
+    strategy = new MapCacheStrategy<CacheEntry, String>();
+  }
+
+  @Test
+  public void testPut() {
+    final CacheEntry key = new CacheEntry("g1", ResourceType.JS, true);
+    final String value = "value";
+    strategy.put(key, value);
+    Assert.assertEquals(value, strategy.get(key));
+  }
+}
---
+++ wro4j-examples/src/main/java/ro/isdc/wro/examples/AppWroManagerFactory.java
@@ -0,0 +1,16 @@
+/*
+ * Copyright (C) 2010 Betfair.
+ * All rights reserved.
+ */
+package ro.isdc.wro.examples;
+
+import ro.isdc.wro.manager.factory.ConfigurableWroManagerFactory;
+
+/**
+ * TODO: DOCUMENT ME!
+ *
+ * @author Alex Objelean
+ */
+public class AppWroManagerFactory extends ConfigurableWroManagerFactory {
+
+}
