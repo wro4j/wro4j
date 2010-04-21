e828d2140dca8e6b6426d4110fd2ae3fc5eaa04b
Alex Objelean <alex.objelean@gmail.com>
2010-04-21 19:32:25 +0300

    add lessCss processor to ExtensionsConfigurableWroManager

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/manager/ExtensionsConfigurableWroManagerFactory.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/manager/ExtensionsConfigurableWroManagerFactory.java
@@ -6,10 +6,12 @@ package ro.isdc.wro.extensions.manager;
 import java.util.Map;
 
 import ro.isdc.wro.extensions.processor.GoogleClosureCompressorProcessor;
+import ro.isdc.wro.extensions.processor.LessCssProcessor;
 import ro.isdc.wro.extensions.processor.YUICssCompressorProcessor;
 import ro.isdc.wro.extensions.processor.YUIJsCompressorProcessor;
 import ro.isdc.wro.manager.factory.ConfigurableWroManagerFactory;
 import ro.isdc.wro.model.resource.processor.ResourcePostProcessor;
+import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 
 import com.google.javascript.jscomp.CompilationLevel;
 
@@ -27,7 +29,16 @@ public class ExtensionsConfigurableWroManagerFactory extends ConfigurableWroMana
   protected void contributePostProcessors(final Map<String, ResourcePostProcessor> map) {
     map.put("yuiCssMin", new YUICssCompressorProcessor());
     map.put("yuiJsMin", new YUIJsCompressorProcessor());
+    map.put("lessCss", new LessCssProcessor());
     map.put("googleClosureSimple", new GoogleClosureCompressorProcessor());
     map.put("googleClosureAdvanced", new GoogleClosureCompressorProcessor(CompilationLevel.ADVANCED_OPTIMIZATIONS));
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void contributePreProcessors(final Map<String, ResourcePreProcessor> map) {
+    map.put("lessCss", new LessCssProcessor());
   }
 }
