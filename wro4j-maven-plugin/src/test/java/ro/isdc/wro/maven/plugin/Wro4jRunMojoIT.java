6ddfe39f61218dfdd01507a21d30348b087cb964
Alex Objelean <alex.objelean@gmail.com>
2010-08-09 18:04:20 +0300

    fix maven plugin classpath resources support issue & make targetGroups parameter optional

---
+++ wro4j-maven-plugin/src/test/java/ro/isdc/wro/maven/plugin/Wro4jRunMojoIT.java
@@ -0,0 +1,64 @@
+/*
+ * Copyright (C) 2010. All rights reserved.
+ */
+package ro.isdc.wro.maven.plugin;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Date;
+
+import org.apache.maven.plugin.testing.AbstractMojoTestCase;
+import org.apache.maven.project.MavenProject;
+import org.codehaus.plexus.util.FileUtils;
+import org.mockito.Mockito;
+
+
+/**
+ * Integration test for wro4j mojo.
+ *
+ * @author Alex Objelean
+ */
+public class Wro4jRunMojoIT
+    extends AbstractMojoTestCase {
+  private File destinationFolder;
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void setUp()
+      throws Exception {
+    super.setUp();
+  }
+
+  /**
+   * @throws Exception
+   */
+  public void testMojoGoal()
+      throws Exception {
+    final File testPom = new File(getBasedir(), "/src/test/resources/unit/1/pom.xml");
+    final Wro4jMojo mojo = (Wro4jMojo) lookupMojo("run", testPom);
+    destinationFolder = new File("wroTemp-" + new Date().getTime());
+    mojo.setDestinationFolder(destinationFolder);
+
+    mojo.setMavenProject(Mockito.mock(MavenProject.class));
+
+    final URL url = getClass().getClassLoader().getResource("wro.xml");
+    final File wroFile = new File(url.toURI());
+    mojo.setWroFile(wroFile);
+    mojo.setContextFolder(wroFile.getParentFile().getParentFile());
+
+    mojo.execute();
+    assertNotNull(mojo);
+  }
+
+  /**
+   * {@inheritDoc}
+   */
+  @Override
+  protected void tearDown()
+      throws Exception {
+    super.tearDown();
+    FileUtils.deleteDirectory(destinationFolder);
+  }
+}
