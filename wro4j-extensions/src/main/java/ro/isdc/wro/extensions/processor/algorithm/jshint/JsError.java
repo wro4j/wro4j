85ce3c0537c31efcce0b33f604cd9de713ef03d0
Alex Objelean <alex.objelean@gmail.com>
2011-03-09 22:00:03 +0200

    remove raw message from JsError

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/algorithm/jshint/JsError.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/algorithm/jshint/JsError.java
@@ -8,9 +8,11 @@ import java.io.Serializable;
 import org.apache.commons.lang.builder.ToStringBuilder;
 import org.apache.commons.lang.builder.ToStringStyle;
 
+
 /**
  * @author Alex Objelean
  */
+@SuppressWarnings("serial")
 public class JsError
   implements Serializable {
   /**
@@ -29,70 +31,71 @@ public class JsError
    * The text line in which the problem occurred
    */
   private String evidence;
-  /**
-   * The raw message before the details were inserted
-   */
-  private String raw;
+
+
   /**
    * @return the line
    */
   public int getLine() {
     return this.line;
   }
+
+
   /**
    * @param line the line to set
    */
   public void setLine(final int line) {
     this.line = line;
   }
+
+
   /**
    * @return the character
    */
   public int getCharacter() {
     return this.character;
   }
+
+
   /**
    * @param character the character to set
    */
   public void setCharacter(final int character) {
     this.character = character;
   }
+
+
   /**
    * @return the reason
    */
   public String getReason() {
     return this.reason;
   }
+
+
   /**
    * @param reason the reason to set
    */
   public void setReason(final String reason) {
     this.reason = reason;
   }
+
+
   /**
    * @return the evidence
    */
   public String getEvidence() {
     return this.evidence;
   }
+
+
   /**
    * @param evidence the evidence to set
    */
   public void setEvidence(final String evidence) {
     this.evidence = evidence;
   }
-  /**
-   * @return the raw
-   */
-  public String getRaw() {
-    return this.raw;
-  }
-  /**
-   * @param raw the raw to set
-   */
-  public void setRaw(final String raw) {
-    this.raw = raw;
-  }
+
 
   /**
    * {@inheritDoc}
