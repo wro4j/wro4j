53e119a860b6bf4701768a22873710fea880649b
Alex Objelean <alex.objelean@gmail.com>
2011-06-15 22:28:46 +0300

    implement PlaceholderInterpoloationProcessor

---
+++ wro4j-core/src/test/java/ro/isdc/wro/model/resource/processor/TestPlaceholderInterpolationProcessor.java
@@ -0,0 +1,47 @@
+/**
+ * Copyright wro4j@2011
+ */
+package ro.isdc.wro.model.resource.processor;
+
+import java.io.File;
+import java.net.URL;
+import java.util.Properties;
+
+import org.junit.Test;
+
+import ro.isdc.wro.WroRuntimeException;
+import ro.isdc.wro.model.resource.processor.impl.PlaceholderInterpolationProcessor;
+import ro.isdc.wro.util.WroTestUtils;
+
+/**
+ * @author Alex Objelean
+ */
+public class TestPlaceholderInterpolationProcessor {
+  @Test
+  public void testProcessor()
+      throws Exception {
+    final Properties properties = new Properties();
+    properties.setProperty("prop1", "value1");
+    properties.setProperty("prop2", "value2");
+    properties.setProperty("prop3", "value3");
+    properties.setProperty("prop4", "value4");
+
+    final ResourcePreProcessor processor = new PlaceholderInterpolationProcessor().setProperties(properties);
+    final URL url = getClass().getResource("placeholderInterpolation");
+
+    final File testFolder = new File(url.getFile(), "test");
+    final File expectedFolder = new File(url.getFile(), "expected");
+    WroTestUtils.compareFromDifferentFoldersByExtension(testFolder, expectedFolder, "css", processor);
+  }
+
+  @Test(expected=WroRuntimeException.class)
+  public void noIgnoreForMissingVariables()
+      throws Exception {
+    final ResourcePreProcessor processor = new PlaceholderInterpolationProcessor().setIgnoreMissingVariables(false);
+    final URL url = getClass().getResource("placeholderInterpolation");
+
+    final File testFolder = new File(url.getFile(), "test");
+    final File expectedFolder = new File(url.getFile(), "expected");
+    WroTestUtils.compareFromDifferentFoldersByExtension(testFolder, expectedFolder, "css", processor);
+  }
+}
