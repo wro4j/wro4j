19f116513a6e19983607d611e8b7dd0442ab4e25
Alex Objelean <alex.objelean@gmail.com>
2011-06-21 18:59:45 +0300

    create CustomWroManager and use it in wro4j-examples with a PlaceholderProcessor

--- wro4j-examples/src/main/java/ro/isdc/wro/examples/manager/CustomWroManager.java
+++ wro4j-examples/src/main/java/ro/isdc/wro/examples/manager/CustomWroManager.java
@@ -4,8 +4,11 @@
  */
 package ro.isdc.wro.examples.manager;
 
+import java.util.Properties;
+
 import ro.isdc.wro.extensions.processor.css.YUICssCompressorProcessor;
 import ro.isdc.wro.manager.factory.BaseWroManagerFactory;
+import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 import ro.isdc.wro.model.resource.processor.factory.ProcessorsFactory;
 import ro.isdc.wro.model.resource.processor.factory.SimpleProcessorsFactory;
 import ro.isdc.wro.model.resource.processor.impl.BomStripperPreProcessor;
@@ -15,6 +18,7 @@ import ro.isdc.wro.model.resource.processor.impl.css.CssUrlRewritingProcessor;
 import ro.isdc.wro.model.resource.processor.impl.css.CssVariablesProcessor;
 import ro.isdc.wro.model.resource.processor.impl.js.JSMinProcessor;
 import ro.isdc.wro.model.resource.processor.impl.js.SemicolonAppenderPreProcessor;
+import ro.isdc.wro.util.ObjectFactory;
 
 /**
  * @author Alex Objelean
@@ -28,7 +32,7 @@ public class CustomWroManager
   @Override
   protected ProcessorsFactory newProcessorsFactory() {
     final SimpleProcessorsFactory factory = new SimpleProcessorsFactory();
-    factory.addPreProcessor(new PlaceholderProcessor());
+    factory.addPreProcessor(getPlaceholderProcessor());
     factory.addPreProcessor(new BomStripperPreProcessor());
     factory.addPreProcessor(new CssImportPreProcessor());
     factory.addPreProcessor(new CssUrlRewritingProcessor());
@@ -38,5 +42,16 @@ public class CustomWroManager
 
     factory.addPostProcessor(new CssVariablesProcessor());
     return factory;
+  }
+
+  private ResourcePreProcessor getPlaceholderProcessor() {
+    return new PlaceholderProcessor().setPropertiesFactory(new ObjectFactory<Properties>() {
+      @Override
+      public Properties create() {
+        final Properties props = new Properties();
+        props.put("GLOBAL_COLOR", "red");
+        return props;
+      }
+    });
   }
 }
