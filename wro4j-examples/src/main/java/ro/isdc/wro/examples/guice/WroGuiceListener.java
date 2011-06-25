525506628e4e101f68d2be37622e3fca237dce17
Alex Objelean <alex.objelean@gmail.com>
2011-06-26 00:12:05 +0300

    move all servlet configurations from web.xml to guice modul config

--- wro4j-examples/src/main/java/ro/isdc/wro/examples/guice/WroGuiceListener.java
+++ wro4j-examples/src/main/java/ro/isdc/wro/examples/guice/WroGuiceListener.java
@@ -1,26 +1,12 @@
 /**
- * Copyright Alex Objelean
+ * Copyright(c)2011
  */
 package ro.isdc.wro.examples.guice;
 
-import java.util.Map;
-
-import javax.servlet.FilterConfig;
-
-import org.apache.commons.lang.StringUtils;
-import org.apache.wicket.protocol.http.ContextParamWebApplicationFactory;
-import org.apache.wicket.protocol.http.WicketFilter;
-import org.directwebremoting.servlet.DwrServlet;
-
-import ro.isdc.wro.examples.WebResourceOptimizationApplication;
-import ro.isdc.wro.http.WroFilter;
-
-import com.google.common.collect.Maps;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
-import com.google.inject.Singleton;
 import com.google.inject.servlet.GuiceServletContextListener;
-import com.google.inject.servlet.ServletModule;
+
 
 /**
  * @author Alex Objelean
@@ -31,33 +17,6 @@ public class WroGuiceListener extends GuiceServletContextListener {
    */
   @Override
   protected Injector getInjector() {
-    return Guice.createInjector(new ServletModule() {
-      @Override
-      protected void configureServlets() {
-        //bindings
-        bind(WroFilter.class).in(Singleton.class);
-        bind(WicketFilter.class).in(Singleton.class);
-        bind(DwrServlet.class).in(Singleton.class);
-
-        //filters
-        //find out how to add dispatchers to the filter mapping configuration
-        filter("/wro/*").through(WroFilter.class);
-
-
-        final String wicketFilterPath = "/*";
-        final Map<String, String> wicketFilterMap = Maps.newHashMap();
-        wicketFilterMap.put(ContextParamWebApplicationFactory.APP_CLASS_PARAM,
-          WebResourceOptimizationApplication.class.getName());
-        filter(wicketFilterPath).through(new WicketFilter() {
-          @Override
-          protected String getFilterPathFromConfig(final FilterConfig filterConfig) {
-            return StringUtils.EMPTY;
-          }
-        }, wicketFilterMap);
-        //servlets
-        serve("/dwr/*").with(DwrServlet.class);
-      }
-    });
+    return Guice.createInjector(new WroExamplesServletModule());
   }
-
 }
