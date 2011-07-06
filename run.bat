d4edb70df613f8fbe35ffd0d1a0153790c868352
Alex Objelean <alex.objelean@gmail.com>
2011-07-07 00:22:08 +0300

    add static assets

---
+++ run.bat
@@ -0,0 +1,9 @@
+rm build/* -f
+rm wro/* -f
+mkdir build
+java -jar wro4j-runner-1.3.8.1-jar-with-dependencies.jar -m -c yuiCssMin --targetGroups all
+cp wro/*.css build
+java -jar wro4j-runner-1.3.8.1-jar-with-dependencies.jar -m -c jsMin --targetGroups all
+cp wro/*.js build
+rm wro -R
+mv build wro
