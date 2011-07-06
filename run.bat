dbe7726b4bf67c10f0c9fdfd12b017405a104241
Alex Objelean <alex.objelean@gmail.com>
2011-07-07 01:13:10 +0300

    update build script

--- run.bat
+++ run.bat
@@ -3,7 +3,7 @@ rm wro/* -f
 mkdir build
 java -jar wro4j-runner-1.3.8.1-jar-with-dependencies.jar -m -c yuiCssMin --targetGroups all
 cp wro/*.css build
-java -jar wro4j-runner-1.3.8.1-jar-with-dependencies.jar -m -c jsMin --targetGroups all
+java -jar wro4j-runner-1.3.8.1-jar-with-dependencies.jar -m -c googleClosureSimple --targetGroups all
 cp wro/*.js build
 rm wro -R
 mv build wro
