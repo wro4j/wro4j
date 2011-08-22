f824a0ad5fd5954077034a0a5c18274f45df31ba
Alex Objelean <alex.objelean@gmail.com>
2011-08-22 16:46:32 +0300

    fix yuiCompressor & stackoverflow exception bug

--- wro4j-examples/wro4j-standalone/src/main/java/ro/isdc/wro/examples/manager/CustomWroManagerFactory.java
+++ wro4j-examples/wro4j-standalone/src/main/java/ro/isdc/wro/examples/manager/CustomWroManagerFactory.java
@@ -4,46 +4,37 @@
  */
 package ro.isdc.wro.examples.manager;
 
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.Properties;
-
-import ro.isdc.wro.config.Context;
-import ro.isdc.wro.extensions.model.factory.GroovyModelFactory;
+import ro.isdc.wro.extensions.processor.css.LessCssProcessor;
 import ro.isdc.wro.extensions.processor.css.YUICssCompressorProcessor;
 import ro.isdc.wro.extensions.processor.js.YUIJsCompressorProcessor;
 import ro.isdc.wro.manager.factory.BaseWroManagerFactory;
-import ro.isdc.wro.model.factory.WroModelFactory;
-import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 import ro.isdc.wro.model.resource.processor.factory.ProcessorsFactory;
 import ro.isdc.wro.model.resource.processor.factory.SimpleProcessorsFactory;
-import ro.isdc.wro.model.resource.processor.impl.PlaceholderProcessor;
 import ro.isdc.wro.model.resource.processor.impl.css.CssImportPreProcessor;
 import ro.isdc.wro.model.resource.processor.impl.css.CssUrlRewritingProcessor;
 import ro.isdc.wro.model.resource.processor.impl.css.CssVariablesProcessor;
 import ro.isdc.wro.model.resource.processor.impl.css.JawrCssMinifierProcessor;
 import ro.isdc.wro.model.resource.processor.impl.js.SemicolonAppenderPreProcessor;
-import ro.isdc.wro.util.ObjectFactory;
 
 /**
  * @author Alex Objelean
  */
 public class CustomWroManagerFactory
     extends BaseWroManagerFactory {
-
-  /**
-   * {@inheritDoc}
-   */
-  @Override
-  protected WroModelFactory newModelFactory() {
-    return new GroovyModelFactory() {
-      @Override
-      protected InputStream getModelResourceAsStream()
-        throws IOException {
-        return Context.get().getServletContext().getResourceAsStream("/WEB-INF/wro.groovy");
-      }
-    };
-  }
+//
+//  /**
+//   * {@inheritDoc}
+//   */
+//  @Override
+//  protected WroModelFactory newModelFactory() {
+//    return new GroovyModelFactory() {
+//      @Override
+//      protected InputStream getModelResourceAsStream()
+//        throws IOException {
+//        return Context.get().getServletContext().getResourceAsStream("/WEB-INF/wro.groovy");
+//      }
+//    };
+//  }
 
   /**
    * {@inheritDoc}
@@ -59,24 +50,24 @@ public class CustomWroManagerFactory
     factory.addPreProcessor(new CssImportPreProcessor());
     factory.addPreProcessor(new CssUrlRewritingProcessor());
     factory.addPreProcessor(new SemicolonAppenderPreProcessor());
+    factory.addPreProcessor(new JawrCssMinifierProcessor());
     //factory.addPreProcessor(new JSMinProcessor());
     factory.addPreProcessor(YUIJsCompressorProcessor.doMungeCompressor());
     factory.addPreProcessor(new YUICssCompressorProcessor());
     factory.addPostProcessor(new CssVariablesProcessor());
-
-    factory.addPreProcessor(new JawrCssMinifierProcessor());
+    factory.addPostProcessor(new LessCssProcessor());
 
     return factory;
   }
-
-  private ResourcePreProcessor getPlaceholderProcessor() {
-    return new PlaceholderProcessor().setPropertiesFactory(new ObjectFactory<Properties>() {
-      @Override
-      public Properties create() {
-        final Properties props = new Properties();
-        props.put("GLOBAL_COLOR", "red");
-        return props;
-      }
-    });
-  }
+//
+//  private ResourcePreProcessor getPlaceholderProcessor() {
+//    return new PlaceholderProcessor().setPropertiesFactory(new ObjectFactory<Properties>() {
+//      @Override
+//      public Properties create() {
+//        final Properties props = new Properties();
+//        props.put("GLOBAL_COLOR", "red");
+//        return props;
+//      }
+//    });
+//  }
 }
