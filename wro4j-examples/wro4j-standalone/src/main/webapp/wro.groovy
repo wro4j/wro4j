8063619be6ee842e2b28a8936ae5f5acc23ce1a0
Alex Objelean <alex.objelean@gmail.com>
2011-08-26 18:42:50 +0300

    add more unit test to wro4j-runner

---
+++ wro4j-examples/wro4j-standalone/src/main/webapp/wro.groovy
@@ -0,0 +1,14 @@
+groups {
+  csslint {
+    js("/libraries/csslint/**")
+  }
+  jshint {
+    js("/libraries/jshint/**")
+  }
+  sass {
+    js("/libraries/sass/**")
+  }
+  uglifyJs {
+    js("/libraries/uglifyJs/**")
+  }
+}
