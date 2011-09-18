0dacfb8142310fa628b0baf6961c94bf4ede1f05
Alex Objelean <alex.objelean@gmail.com>
2011-09-18 11:10:47 +0300

    update to latest coffeeScriptversion

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/uglify/UglifyJs.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/uglify/UglifyJs.java
@@ -20,7 +20,8 @@ import ro.isdc.wro.util.WroUtil;
 
 
 /**
- * The underlying implementation use the uglifyJs version commited at the following date: 2011-09-18 08:14:44.
+ * The underlying implementation use the uglifyJs version (1.0.8 snapshot) <br/>
+ * commited at the following date: 2011-09-18 08:14:44.
  * <p/>
  * {@link https://github.com/mishoo/UglifyJS}.
  * <p/>
@@ -35,7 +36,7 @@ public class UglifyJs {
   /**
    * The name of the uglify script to be used by default.
    */
-  private static final String DEFAULT_UGLIFY_JS = "uglify.js";
+  private static final String DEFAULT_UGLIFY_JS = "uglify-1.0.8-pre.js";
   /**
    * If true, the script is uglified, otherwise it is beautified.
    */
