7efed7815855bc3fa7292f1ab06e4c808d650ba2
Alex Objelean <alex.objelean@gmail.com>
2011-10-13 19:19:37 +0300

    trying to fix wildcard strange issue

--- wro4j-core/src/test/java/ro/isdc/wro/model/factory/TestWildcardExpanderModelTransformer.java
+++ wro4j-core/src/test/java/ro/isdc/wro/model/factory/TestWildcardExpanderModelTransformer.java
@@ -120,6 +120,24 @@ public class TestWildcardExpanderModelTransformer {
     Assert.assertEquals(resultPathPrefix + "/folder2/script2.js", changedModel.getGroupByName("group").getResources().get(1).getUri());
   }
 
+
+  @Test
+  public void wildcardResourcesAreOrderedAlphabetically() {
+    final WroModel model = new WroModel();
+    final String uri = String.format(ClasspathUriLocator.PREFIX + "%s/expander/order1/**.js", WroUtil.toPackageAsFolder(getClass()));
+    model.addGroup(new Group("group").addResource(Resource.create(uri, ResourceType.JS)));
+    Mockito.when(decoratedFactory.create()).thenReturn(model);
+
+    final WroModel changedModel = transformer.transform(model);
+    LOG.debug("model: {}", changedModel);
+
+    final String resultPathPrefix = String.format("%s%s/expander/order", ClasspathUriLocator.PREFIX, WroUtil.toPackageAsFolder(getClass()));
+
+    Assert.assertEquals(2, changedModel.getGroupByName("group").getResources().size());
+    Assert.assertEquals(resultPathPrefix + "/folder1/script1.js", changedModel.getGroupByName("group").getResources().get(0).getUri());
+    Assert.assertEquals(resultPathPrefix + "/folder2/script2.js", changedModel.getGroupByName("group").getResources().get(1).getUri());
+  }
+
   @After
   public void tearDown() {
     Context.unset();
