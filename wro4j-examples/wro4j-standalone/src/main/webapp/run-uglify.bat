924015b08a09d5bc9986c67339fbff9fa591bf76
Alex Objelean <alex.objelean@gmail.com>
2011-12-05 16:03:22 +0200

    the available groups message is a comma separated list of group names (not just groups toString)

--- wro4j-examples/wro4j-standalone/src/main/webapp/run-uglify.bat
+++ wro4j-examples/wro4j-standalone/src/main/webapp/run-uglify.bat
@@ -1,3 +1,3 @@
-java -jar ../../../../../wro4j-runner/target/wro4j-runner-1.4.2-SNAPSHOT-jar-with-dependencies.jar -m -c semicolonAppender,uglifyJs --targetGroups uglifyJs 
+java -jar ../../../../../wro4j-runner/target/wro4j-runner-1.4.2-jar-with-dependencies.jar -m -c semicolonAppender,uglifyJs --targetGroups uglifyJs 
 
 
