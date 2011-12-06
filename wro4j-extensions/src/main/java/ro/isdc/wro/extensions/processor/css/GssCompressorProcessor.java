5d5192ec9712479f9dd0c5c3ab34de83157dc9b7
Alex Objelean <alex.objelean@gmail.com>
2011-12-06 11:52:37 +0200

    update groupId for google closure stylesheet dependency

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/css/GssCompressorProcessor.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/css/GssCompressorProcessor.java
@@ -24,101 +24,96 @@ import com.google.common.css.JobDescription;
 import com.google.common.css.JobDescriptionBuilder;
 import com.google.common.css.SourceCode;
 import com.google.common.css.compiler.ast.ErrorManager;
-import com.google.common.css.compiler.ast.GssError;
 import com.google.common.css.compiler.ast.GssParserException;
 import com.google.common.css.compiler.ast.PrintStreamErrorManager;
 import com.google.common.css.compiler.commandline.DefaultCommandLineCompiler;
 
+
 /**
- * Uses Google closure compiler for css minimization and optimization.
+ * Uses Google closure stylesheet for css minimization and optimization.
  */
 @Minimize
 @SupportedResourceType(ResourceType.CSS)
-public class GssCompressorProcessor implements ResourcePostProcessor,
-		ResourcePreProcessor {
-
-	private static final Logger LOG = LoggerFactory.getLogger(GssCompressorProcessor.class);
-
-	private static class WroGssCompiler extends DefaultCommandLineCompiler {
-
-		private WroGssCompiler(JobDescription job,
-				ExitCodeHandler exitCodeHandler, ErrorManager errorManager) {
-			super(job, exitCodeHandler, errorManager);
-		}
-	}
-
-	private final ErrorManager errorManager;
-
-	public GssCompressorProcessor() {
-		this(new PrintStreamErrorManager(System.err));
-	}
-
-	/**
-	 */
-	public GssCompressorProcessor(final ErrorManager errorManager) {
-		
-		this.errorManager = Validate.notNull(errorManager);
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public void process(final Resource resource, final Reader reader,
-			final Writer writer) throws IOException {
-		final String content = IOUtils.toString(reader);
-		try {
-
-			try {
-				String fileName = resource == null ? "default-gss.css" : FilenameUtils.getName(resource.getUri());
-				
-				JobDescription job = new JobDescriptionBuilder()
-					.addInput(new SourceCode(fileName, content))
-					
-					// 
-					.setSimplifyCss(true)
-					
-					// merge identical rules 
-					.setEliminateDeadStyles(true)
-					
-					// allow -moz, -webkit, -o, ... 
-					.setAllowUnrecognizedFunctions(true)
-					
-					.setAllowedNonStandardFunctions(Arrays.asList("-webkit-linear-gradient()", 
-							"-webkit-gradient()", "from()", "to()"))
-					
-					.setProcessDependencies(true)
-					
-					.getJobDescription();
-
-				WroGssCompiler compiler = new WroGssCompiler(job, new DefaultExitCodeHandler(),
-						errorManager);
-				
-				String compiled = compiler.compile();
-
-				if (errorManager.hasErrors()) {
-					writer.write(content);
-				} else {
-					writer.write(compiled);
-				}
-			} catch (GssParserException gpx) {
-				LOG.error(gpx.getMessage(), gpx);
-				writer.write(content);
-			}
-
-		} finally {
-			LOG.debug("finally");
-			reader.close();
-			writer.close();
-		}
-	}
-
-	/**
-	 * {@inheritDoc}
-	 */
-	public void process(final Reader reader, final Writer writer)
-			throws IOException {
-
-		process(null, reader, writer);
-	}
-
+public class GssCompressorProcessor
+        implements ResourcePostProcessor, ResourcePreProcessor {
+    
+    private static final Logger LOG = LoggerFactory.getLogger(GssCompressorProcessor.class);
+    
+    private static class WroGssCompiler
+            extends DefaultCommandLineCompiler {
+        
+        private WroGssCompiler(JobDescription job, ExitCodeHandler exitCodeHandler, ErrorManager errorManager) {
+            super(job, exitCodeHandler, errorManager);
+        }
+    }
+    
+    private final ErrorManager errorManager;
+    
+    public GssCompressorProcessor() {
+        this(new PrintStreamErrorManager(System.err));
+    }
+    
+    public GssCompressorProcessor(final ErrorManager errorManager) {
+        this.errorManager = Validate.notNull(errorManager);
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void process(final Resource resource, final Reader reader, final Writer writer)
+            throws IOException {
+        final String content = IOUtils.toString(reader);
+        try {
+            
+            try {
+                String fileName = resource == null ? "default-gss.css" : FilenameUtils.getName(resource.getUri());
+                
+                JobDescription job = new JobDescriptionBuilder().addInput(new SourceCode(fileName, content))
+                
+                //
+                .setSimplifyCss(true)
+                
+                // merge identical rules
+                .setEliminateDeadStyles(true)
+                
+                // allow -moz, -webkit, -o, ...
+                .setAllowUnrecognizedFunctions(true)
+                
+                .setAllowedNonStandardFunctions(
+                        Arrays.asList("-webkit-linear-gradient()", "-webkit-gradient()", "from()", "to()"))
+                
+                .setProcessDependencies(true)
+                
+                .getJobDescription();
+                
+                WroGssCompiler compiler = new WroGssCompiler(job, new DefaultExitCodeHandler(), errorManager);
+                
+                String compiled = compiler.compile();
+                if (errorManager.hasErrors()) {
+                    LOG.debug("Has Errors!");
+                    errorManager.generateReport();
+                    writer.write(content);
+                } else {
+                    writer.write(compiled);
+                }
+            } catch (GssParserException gpx) {
+                LOG.error(gpx.getMessage(), gpx);
+                writer.write(content);
+            }
+            
+        } finally {
+            LOG.debug("finally");
+            reader.close();
+            writer.close();
+        }
+    }
+    
+    /**
+     * {@inheritDoc}
+     */
+    public void process(final Reader reader, final Writer writer)
+            throws IOException {
+        process(null, reader, writer);
+    }
+    
 }
