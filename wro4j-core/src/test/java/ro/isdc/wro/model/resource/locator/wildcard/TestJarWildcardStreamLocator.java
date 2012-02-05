ec00a5778234b2ab7534270aabdb49a4be5f102b
Alex Objelean <alex.objelean@gmail.com>
2012-02-05 12:47:36 +0200

    add debub level logging

--- wro4j-core/src/test/java/ro/isdc/wro/model/resource/locator/wildcard/TestJarWildcardStreamLocator.java
+++ wro4j-core/src/test/java/ro/isdc/wro/model/resource/locator/wildcard/TestJarWildcardStreamLocator.java
@@ -110,10 +110,35 @@ public class TestJarWildcardStreamLocator {
     jarStreamLocator.locateStream("com/test/app/*.js", new File("test.jpg"));
   }
 
+  @Test
+  public void shouldFindNoResourcesWhenNoneExist() throws IOException {
+    final ThreadLocal<Collection<String>> filenameListHolder = new ThreadLocal<Collection<String>>();
+    final UriLocator uriLocator = createJarLocator(filenameListHolder);
+    //there are no js resources in the jar
+    uriLocator.locate("classpath:com/**.js");
+    final Collection<String> filenameList = filenameListHolder.get();
+    Assert.assertNotNull(filenameList);
+    Assert.assertTrue(filenameList.isEmpty());
+  }
 
   @Test
-  public void testWildcardResourcesOrderedAlphabetically() throws IOException {
+  public void shouldOrderedAlphabeticallyWildcardResources() throws IOException {
     final ThreadLocal<Collection<String>> filenameListHolder = new ThreadLocal<Collection<String>>();
+    final UriLocator uriLocator = createJarLocator(filenameListHolder);
+    uriLocator.locate("classpath:com/**.css");
+    final Collection<String> filenameList = filenameListHolder.get();
+    Assert.assertNotNull(filenameList);
+    Assert.assertEquals(Arrays.toString(new String[] {
+      "com/app/level1/level2/styles/style.css", "com/app/level1/level2/level2.css", "com/app/level1/level1.css"
+    }), Arrays.toString(filenameList.toArray()));
+  }
+
+
+  /**
+   * @param filenameListHolder
+   * @return
+   */
+  private UriLocator createJarLocator(final ThreadLocal<Collection<String>> filenameListHolder) {
     jarStreamLocator = new JarWildcardStreamLocator() {
       @Override
       File getJarFile(final File folder) {
@@ -136,12 +161,7 @@ public class TestJarWildcardStreamLocator {
         return jarStreamLocator;
       }
     };
-    uriLocator.locate("classpath:com/**.css");
-    final Collection<String> filenameList = filenameListHolder.get();
-    Assert.assertNotNull(filenameList);
-    Assert.assertEquals(Arrays.toString(new String[] {
-      "com/app/level1/level2/styles/style.css", "com/app/level1/level2/level2.css", "com/app/level1/level1.css"
-    }), Arrays.toString(filenameList.toArray()));
+    return uriLocator;
   }
 
 }
