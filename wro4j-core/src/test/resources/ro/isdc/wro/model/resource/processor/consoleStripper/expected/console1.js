c058ff5857ad7a68d6df402268267c4b75910c43
Ivar Conradi Østhus <ivarconr@gmail.com>
2012-03-01 19:54:20 +0100

    Added a preProcessor which removes console.log|debug|info|count statements

---
+++ wro4j-core/src/test/resources/ro/isdc/wro/model/resource/processor/consoleStripper/expected/console1.js
@@ -0,0 +1,6 @@
+function f1() {
+  var a = 1, b = 2, c;
+  
+  c = a + b;
+  c = c + 2;
+}
---
+++ wro4j-core/src/test/resources/ro/isdc/wro/model/resource/processor/consoleStripper/test/console1.js
@@ -0,0 +1,7 @@
+function f1() {
+  var a = 1, b = 2, c;
+  console.debug("hello world");
+  c = a + b;
+  console.log(c);c = c + 2;
+}
+console.info("completed");
