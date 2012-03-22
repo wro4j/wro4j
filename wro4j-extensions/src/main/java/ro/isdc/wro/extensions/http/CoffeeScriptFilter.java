ebead2814dbc672b7626eef0717c6afd2eb7f1cb
Alex Objelean <alex.objelean@gmail.com>
2012-03-23 00:11:53 +0200

    create LessCssFilter & CoffeScriptFilter. Add unit test.

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/http/CoffeeScriptFilter.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/http/CoffeeScriptFilter.java
@@ -3,23 +3,23 @@ package ro.isdc.wro.extensions.http;
 import java.util.ArrayList;
 import java.util.List;
 
-import ro.isdc.wro.extensions.processor.css.LessCssProcessor;
+import ro.isdc.wro.extensions.processor.js.CoffeeScriptProcessor;
 import ro.isdc.wro.http.support.AbstractProcessorsFilter;
 import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 
 /**
- * A filter which transforms a less resource into css.
+ * A filter which transforms a coffeeScript resource into javascript.
  * 
  * @author Alex Objelean
  * @since 1.4.5
- * @created 18 Mar 2012
+ * @created 22 Mar 2012
  */
-public class LessCssFilter
+public class CoffeeScriptFilter
     extends AbstractProcessorsFilter {
   private List<ResourcePreProcessor> list = new ArrayList<ResourcePreProcessor>();
 
-  public LessCssFilter() {
-    list.add(new LessCssProcessor());
+  public CoffeeScriptFilter() {
+    list.add(new CoffeeScriptProcessor());
   }
   
   /**
