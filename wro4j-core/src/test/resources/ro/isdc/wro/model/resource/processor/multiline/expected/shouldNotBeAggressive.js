a23bac9065a1c6a64827e05cc1884db344e145c8
Alex Objelean <alex.objelean@gmail.com>
2012-03-29 00:34:17 +0300

    trying to reproduce the bug

---
+++ wro4j-core/src/test/resources/ro/isdc/wro/model/resource/processor/multiline/expected/shouldNotBeAggressive.js
@@ -0,0 +1,4 @@
+/**
+ * The script contains a /* sequence which should not be removed. 
+ */
+J.setRequestHeader("Accept", M.dataType && M.accepts[M.dataType] ? M.accepts[M.dataType] + ", */*" : M.accepts._default)
