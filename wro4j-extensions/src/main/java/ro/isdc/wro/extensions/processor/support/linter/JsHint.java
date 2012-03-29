559ae15c6ac4f0f92f056ce270d878636a0f2b99
Alex Objelean <alex.objelean@gmail.com>
2012-03-29 19:20:43 +0300

    use minimized jsHint version & update javadoc

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/linter/JsHint.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/linter/JsHint.java
@@ -9,7 +9,7 @@ import java.io.InputStream;
 /**
  * Apply JsHint script checking utility.
  * <p/>
- * Using untagged version (commited: 2012-02-17 10:30:06)
+ * Using untagged version (commited: 2012-03-28 22:19:31)
  *
  * @author Alex Objelean
  * @since 1.3.5
@@ -18,7 +18,7 @@ public class JsHint extends AbstractLinter {
   /**
    * The name of the jshint script to be used by default.
    */
-  private static final String DEFAULT_JSHINT_JS = "jshint.js";
+  private static final String DEFAULT_JSHINT_JS = "jshint.min.js";
 
   /**
    * @return the stream of the jshint script. Override this method to provide a different script version.
