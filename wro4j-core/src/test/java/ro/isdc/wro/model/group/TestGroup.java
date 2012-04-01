3608e8d1cb0e6bf0bb9b5feaab7bd10546775713
Alex Objelean <alex.objelean@gmail.com>
2012-04-01 14:30:08 +0300

    add used flag to group

--- wro4j-core/src/test/java/ro/isdc/wro/model/group/TestGroup.java
+++ wro4j-core/src/test/java/ro/isdc/wro/model/group/TestGroup.java
@@ -11,39 +11,35 @@ import java.util.List;
 import junit.framework.Assert;
 
 import org.junit.Test;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import ro.isdc.wro.model.resource.Resource;
 import ro.isdc.wro.model.resource.ResourceType;
 
+
 /**
  * Test class for {@link Group}.
- *
+ * 
  * @author Alex Objelean
  */
 public class TestGroup {
-  private static final Logger LOG = LoggerFactory.getLogger(TestGroup.class);
-
-  @Test(expected=NullPointerException.class)
+  @Test(expected = NullPointerException.class)
   public void cannotCreateGroupWithNullName() {
     new Group(null);
   }
-
-  @Test(expected=NullPointerException.class)
+  
+  @Test(expected = NullPointerException.class)
   public void cannotPassNullResourceType() {
     final Group group = new Group("group");
     group.hasResourcesOfType(null);
   }
-
-
+  
   @Test
   public void testNoResorucesOfTypeFound() {
     final Group group = new Group("group");
     Assert.assertEquals(false, group.hasResourcesOfType(ResourceType.CSS));
     Assert.assertEquals(false, group.hasResourcesOfType(ResourceType.JS));
   }
-
+  
   @Test
   public void testResoruceOfTypeFound() {
     final Group group = new Group("group");
@@ -53,13 +49,13 @@ public class TestGroup {
     Assert.assertEquals(true, group.hasResourcesOfType(ResourceType.CSS));
     Assert.assertEquals(false, group.hasResourcesOfType(ResourceType.JS));
   }
-
-  @Test(expected=IllegalArgumentException.class)
+  
+  @Test(expected = IllegalArgumentException.class)
   public void cannotReplaceMissingResource() {
     final Group group = new Group("group");
     group.replace(Resource.create("/path", ResourceType.JS), Arrays.asList(Resource.create("", ResourceType.JS)));
   }
-
+  
   @SuppressWarnings("unchecked")
   @Test
   public void testReplaceWithEmptyCollection() {
@@ -69,7 +65,7 @@ public class TestGroup {
     group.replace(resource, Collections.EMPTY_LIST);
     Assert.assertTrue(group.getResources().isEmpty());
   }
-
+  
   @Test
   public void testReplaceWithFewResources() {
     final Group group = new Group("group");
@@ -83,18 +79,31 @@ public class TestGroup {
     Assert.assertEquals(2, group.getResources().size());
     Assert.assertEquals(resource.isMinimize(), group.getResources().get(0).isMinimize());
   }
-
+  
+  @Test
+  public void shouldBeNotUsedByDefault() {
+    final Group group = new Group("group");
+    Assert.assertFalse(group.isUsed());
+  }
+  
+  @Test
+  public void shouldBeUsedAfterMarkedAsUsed() {
+    final Group group = new Group("group");
+    group.markAsUsed();
+    Assert.assertTrue(group.isUsed());
+  }
+  
   @Test
   public void shouldReplaceOnlyOneAndPreserveOtherResources() {
     final Group group = new Group("group");
     final Resource resource = Resource.create("/static/*", ResourceType.JS);
-
+    
     final Resource r0 = Resource.create("/asset/1.js", ResourceType.JS);
     group.addResource(r0);
-
+    
     final Resource r1 = Resource.create("/asset/2.js", ResourceType.JS);
     group.addResource(r1);
-
+    
     group.addResource(resource);
     group.replace(
         resource,
