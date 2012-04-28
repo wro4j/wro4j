74c73aefe2bca832abaf94db097fc77a3218d77a
Alex Objelean <alex.objelean@gmail.com>
2012-04-28 15:41:09 +0300

    release profile should be optional ional and rollback jsMultiline utility method changes

--- wro4j-core/src/main/java/ro/isdc/wro/util/WroUtil.java
+++ wro4j-core/src/main/java/ro/isdc/wro/util/WroUtil.java
@@ -225,13 +225,7 @@ public final class WroUtil {
         result.append(",");
       }
     }
-    /**
-     * Probably we should use System.getProperty("line.separator"), but it is difficult to get system independent
-     * carriage return literally. Use windows specific crlf, because it works in windows, otherwise it won't work on
-     * windows only: (@see issue422)
-     */
-    final String lineSeparator = "\\r\\n";
-    result.append("].join(\"" + lineSeparator + "\")");
+    result.append("].join(\"\\n\")");
     return result.toString();
   }
 
