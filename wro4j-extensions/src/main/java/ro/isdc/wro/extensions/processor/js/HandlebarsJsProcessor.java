8ec6f69be7cdb24eada632a07b51c293a00fdc16
Alex Objelean <alex.objelean@gmail.com>
2012-05-26 17:30:44 +0300

    update javadoc and add missing annotation

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/HandlebarsJsProcessor.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/HandlebarsJsProcessor.java
@@ -13,36 +13,40 @@ import org.apache.commons.io.IOUtils;
 import ro.isdc.wro.extensions.processor.support.ObjectPoolHelper;
 import ro.isdc.wro.extensions.processor.support.handlebarsjs.HandlebarsJs;
 import ro.isdc.wro.model.resource.Resource;
+import ro.isdc.wro.model.resource.ResourceType;
+import ro.isdc.wro.model.resource.SupportedResourceType;
 import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
 import ro.isdc.wro.util.ObjectFactory;
 
 
 /**
- * This {@link ResourcePreProcessor} compiles HandlebarsJS templates to javascript.
+ * Compiles HandlebarsJS templates to javascript.
  *
  * @author heldeen
+ * @since 1.4.7
  */
+@SupportedResourceType(ResourceType.JS)
 public class HandlebarsJsProcessor
     implements ResourcePreProcessor {
-  public static final String ALIAS = "HandlebarsJs";
+  public static final String ALIAS = "handlebarsJs";
 
   private final ObjectPoolHelper<HandlebarsJs> enginePool;
 
   public HandlebarsJsProcessor() {
     enginePool = new ObjectPoolHelper<HandlebarsJs>(new ObjectFactory<HandlebarsJs>() {
-
       @Override
       public HandlebarsJs create() {
         return new HandlebarsJs();
       }
-
     });
   }
 
+  /**
+   * {@inheritDoc}
+   */
   @Override
   public void process(final Resource resource, final Reader reader, final Writer writer)
       throws IOException {
-
     final String content = IOUtils.toString(reader);
     final HandlebarsJs handlebarsJs = enginePool.getObject();
     final String name = resource == null ? "" : FilenameUtils.getBaseName(resource.getUri());
@@ -56,6 +60,10 @@ public class HandlebarsJsProcessor
     }
   }
 
+
+  /**
+   * {@inheritDoc}
+   */
   public void process(final Reader reader, final Writer writer)
       throws IOException {
     process(null, reader, writer);
