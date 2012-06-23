101b35bd81a362f77d159ef950916e049b255b8b
Alex Objelean <alex.objelean@gmail.com>
2012-06-23 19:28:03 +0300

    fix issue469: add new method to model which helps to identify the groups to which a resource belongs to

--- wro4j-core/src/main/java/ro/isdc/wro/model/WroModel.java
+++ wro4j-core/src/main/java/ro/isdc/wro/model/WroModel.java
@@ -67,6 +67,22 @@ public final class WroModel {
     return this;
   }
 
+  /**
+   * @param resource
+   *          the {@link Resource} to search in all available groups.
+   * @return t collection of group names containing provided resource. If the resource is not availalbe, an empty collection
+   *         will be returned.
+   */
+  public Collection<String> getGroupNamesContainingResource(final String resourceUri) {
+    Validate.notNull(resourceUri);
+    final Set<String> groupNames = new HashSet<String>();
+    for (Group group : getGroups()) {
+      if (group.hasResource(resourceUri)) {
+        groupNames.add(group.getName());
+      }
+    }
+    return groupNames;
+  }
 
   /**
    * Identify duplicate group names.
