7e48cf2732485924474df0937100b0a155924745
Marvin B. Lillehaug <marlil@kantega.no>
2012-09-18 12:22:49 +0200

    When the last line in css does not end with ;, eg. none, CSSMin replaced this with non;

--- wro4j-core/src/test/java/ro/isdc/wro/model/resource/processor/TestCssMinProcessor.java
+++ wro4j-core/src/test/java/ro/isdc/wro/model/resource/processor/TestCssMinProcessor.java
@@ -3,15 +3,15 @@
  */
 package ro.isdc.wro.model.resource.processor;
 
-import java.io.File;
-import java.net.URL;
-
+import org.apache.commons.io.IOUtils;
 import org.junit.Test;
-
 import ro.isdc.wro.model.resource.ResourceType;
 import ro.isdc.wro.model.resource.processor.decorator.ExceptionHandlingProcessorDecorator;
 import ro.isdc.wro.model.resource.processor.impl.css.CssMinProcessor;
 import ro.isdc.wro.util.WroTestUtils;
+
+import java.io.*;
+import java.net.URL;
 
 
 /**
@@ -45,5 +45,21 @@ public class TestCssMinProcessor {
   @Test
   public void shouldSupportCorrectResourceTypes() {
     WroTestUtils.assertProcessorSupportResourceTypes(new CssMinProcessor(), ResourceType.CSS);
+  }
+
+  @Test
+  public void shouldNotRemoveLastCharWhenAddingSemiColon() throws IOException {
+      ResourcePostProcessor processor = new CssMinProcessor();
+      URL url = getClass().getResource("cssmin");
+
+      File testFile = new File(url.getFile(), "test/addingSemi.css");
+      File expectedFile = new File(url.getFile(), "expected/addingSemi.css");
+      StringWriter minifiedWriter = new StringWriter();
+      processor.process(new FileReader(testFile), minifiedWriter);
+
+      String minified = minifiedWriter.toString();
+
+      String expected = IOUtils.toString(new FileInputStream(expectedFile));
+      WroTestUtils.compare(expected, minified);
   }
 }
