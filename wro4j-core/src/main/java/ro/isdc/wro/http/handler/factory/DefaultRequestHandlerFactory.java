cab0402a0a06c61479d7250795deff8c41b9f7ef
Alex Objelean <alex.objelean@gmail.com>
2012-09-23 13:29:17 +0300

    update DefaultRequestHandlerFactory implementation to use spi

--- wro4j-core/src/main/java/ro/isdc/wro/http/handler/factory/DefaultRequestHandlerFactory.java
+++ wro4j-core/src/main/java/ro/isdc/wro/http/handler/factory/DefaultRequestHandlerFactory.java
@@ -1,22 +1,19 @@
 package ro.isdc.wro.http.handler.factory;
 
 import java.util.ArrayList;
-import java.util.Collection;
+import java.util.List;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import ro.isdc.wro.http.handler.ReloadCacheRequestHandler;
-import ro.isdc.wro.http.handler.ReloadModelRequestHandler;
 import ro.isdc.wro.http.handler.RequestHandler;
-import ro.isdc.wro.http.handler.ResourceProxyRequestHandler;
+import ro.isdc.wro.http.handler.spi.RequestHandlerProvider;
+import ro.isdc.wro.util.provider.ProviderFinder;
 
 
 /**
- * Default {@link RequestHandlerFactory} which provides the the following handlers: {@link ReloadCacheRequestHandler} &
- * {@link ReloadModelRequestHandler}.
- *
- * WroModelAsJsonRequestHandler is added only if the wro4j-extensions library is loaded.
+ * Default {@link RequestHandlerFactory} which adds all {@link RequestHandler}'s provided as spi by
+ * {@link RequestHandlerProvider} found in classpath.
  * 
  * @author Ivar Conradi Østhus
  * @created 19 May 2012
@@ -25,33 +22,17 @@ import ro.isdc.wro.http.handler.ResourceProxyRequestHandler;
 public class DefaultRequestHandlerFactory extends SimpleRequestHandlerFactory {
 
   private static final Logger LOG = LoggerFactory.getLogger(DefaultRequestHandlerFactory.class);
-
   /**
    * Creates a factory with a list of default handlers.
    */
   public DefaultRequestHandlerFactory() {
     // TODO use provider to load all available handlers as default behavior.
-    Collection<RequestHandler> requestHandlers = new ArrayList<RequestHandler>();
-    requestHandlers.add(new ResourceProxyRequestHandler());
-    requestHandlers.add(new ReloadCacheRequestHandler());
-    requestHandlers.add(new ReloadModelRequestHandler());
-    addExtensionsHandlers(requestHandlers);
-    setHandlers(requestHandlers);
-  }
-
-  /**
-   * Add more handlers via reflection. If the module containing this handler is not available, it won't be added.
-   */
-  private void addExtensionsHandlers(final Collection<RequestHandler> requestHandlers) {
-    try {
-      // TODO add a test in extension module which proves that this handler is added when the the extensions module is
-      // available.
-      RequestHandler requestHandler = (RequestHandler) Class.forName(
-          "ro.isdc.wro.extensions.http.handler.ModelAsJsonRequestHandler").newInstance();
-      requestHandlers.add(requestHandler);
-    } catch (final Exception e) {
-      LOG.debug("ModelAsJsonRequestHandler not found, probably because wro4j-extensions "
-          + "module is missing from the classpath.");
+    final List<RequestHandlerProvider> requestHandlerProviders = ProviderFinder.of(RequestHandlerProvider.class).find();
+    final List<RequestHandler> requestHandlers = new ArrayList<RequestHandler>();
+    for (RequestHandlerProvider provider : requestHandlerProviders) {
+      LOG.debug("using provider: {}", provider);
+      requestHandlers.addAll(provider.provideRequestHandlers().values());
     }
+    setHandlers(requestHandlers);
   }
 }
