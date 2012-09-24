f4ed0b75f4d0bbb1dafb6d69a8cf2d318a3641ca
Alex Objelean <alex.objelean@gmail.com>
2012-09-24 19:56:58 +0300

    update demo example to retrieve a cache value outside the WroFilter

--- wro4j-examples/wro4j-demo/src/main/java/ro/isdc/wro/examples/ExternalResourceServlet.java
+++ wro4j-examples/wro4j-demo/src/main/java/ro/isdc/wro/examples/ExternalResourceServlet.java
@@ -13,8 +13,14 @@ import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import ro.isdc.wro.cache.CacheEntry;
+import ro.isdc.wro.cache.ContentHashEntry;
 import ro.isdc.wro.http.support.ServletContextAttributeHelper;
+import ro.isdc.wro.manager.WroManager;
+import ro.isdc.wro.manager.factory.InjectableWroManagerFactoryDecorator;
+import ro.isdc.wro.manager.factory.WroManagerFactory;
 import ro.isdc.wro.model.WroModel;
+import ro.isdc.wro.model.resource.ResourceType;
 
 /**
  * @author Alex Objelean
@@ -27,10 +33,17 @@ public class ExternalResourceServlet extends HttpServlet {
   @Override
   protected void doGet(final HttpServletRequest req, final HttpServletResponse resp)
     throws ServletException, IOException {
-    ServletContextAttributeHelper helper = new ServletContextAttributeHelper(getServletContext());
-
-    WroModel model = helper.getManagerFactory().create().getModelFactory().create();
-    LOG.debug("model: " + model);
+    final ServletContextAttributeHelper helper = new ServletContextAttributeHelper(getServletContext());
+    final WroManagerFactory managerFactory = new InjectableWroManagerFactoryDecorator(helper.getManagerFactory());
+    System.out.println("managerFactory: " + managerFactory);
+    final WroManager wroManager = managerFactory.create();
+    
+    final CacheEntry cacheKey = new CacheEntry("login", ResourceType.CSS, false);
+    ContentHashEntry cacheValue = wroManager.getCacheStrategy().get(cacheKey);
+    LOG.info("cacheValue: {}", cacheValue);
+    
+    final WroModel model = wroManager.getModelFactory().create();
+    LOG.debug("model: {}", model);
     resp.sendRedirect("http://code.jquery.com/jquery-1.4.4.js");
   }
 }
