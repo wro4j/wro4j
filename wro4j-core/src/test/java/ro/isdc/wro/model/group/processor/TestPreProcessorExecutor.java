7685a0dbbfc483291079ba2124aabfa88a4287af
Alex Objelean <alex.objelean@gmail.com>
2012-10-17 20:41:37 +0300

    fix failing unit test

--- wro4j-core/src/test/java/ro/isdc/wro/model/group/processor/TestPreProcessorExecutor.java
+++ wro4j-core/src/test/java/ro/isdc/wro/model/group/processor/TestPreProcessorExecutor.java
@@ -200,6 +200,7 @@ public class TestPreProcessorExecutor {
   @Test(expected = WroRuntimeException.class)
   public void shouldFailWhenUsingFailingPreProcessor()
       throws Exception {
+    Context.get().getConfig().setIgnoreFailingProcessor(false);
     useFailingPreProcessor();
   }
 
