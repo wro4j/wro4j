2394908edbd529370ba08aa65a31ad5cfc861603
Alex Objelean <alex.objelean@gmail.com>
2012-11-04 16:19:04 +0200

    use system property to configure dustJs compiler file path

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/dustjs/DustJs.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/dustjs/DustJs.java
@@ -1,6 +1,10 @@
 package ro.isdc.wro.extensions.processor.support.dustjs;
 
+import java.io.FileInputStream;
+import java.io.IOException;
 import java.io.InputStream;
+
+import org.apache.commons.lang3.StringUtils;
 
 import ro.isdc.wro.extensions.processor.support.template.AbstractJsTemplateCompiler;
 
@@ -14,14 +18,21 @@ import ro.isdc.wro.extensions.processor.support.template.AbstractJsTemplateCompi
  * @created 8 Mar 2012
  */
 public class DustJs extends AbstractJsTemplateCompiler {
+  /**
+   * The system property name used to specify the path of the dust compiler. If this is not specified, the default
+   * compiler is used.
+   */
+  private static final String SYSTEM_PROPERTY_NAME = "dustJs.file.path";
   private static final String DEFAULT_DUST_JS = "dust-full-1.1.1.min.js";
 
   /**
    * {@inheritDoc}
    */
   @Override
-  protected InputStream getCompilerAsStream() {
-    return DustJs.class.getResourceAsStream(DEFAULT_DUST_JS);
+  protected InputStream getCompilerAsStream() throws IOException {
+    final String compilerPath = System.getProperty(SYSTEM_PROPERTY_NAME, DEFAULT_DUST_JS);
+    return StringUtils.isEmpty(compilerPath) ? DustJs.class.getResourceAsStream(DEFAULT_DUST_JS) : new FileInputStream(
+        compilerPath);
   }
 
   /**
--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/template/AbstractJsTemplateCompiler.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/template/AbstractJsTemplateCompiler.java
@@ -12,17 +12,17 @@ import ro.isdc.wro.util.WroUtil;
 
 /**
  * A base class for template processors like: dustJS or hoganJS.
- *  
+ *
  * @author Eivind Barstad Waaler
  * @since 1.4.7
  * @created 11 May 2012
  */
 public abstract class AbstractJsTemplateCompiler {
   private ScriptableObject scope;
-  
+
   /**
    * Compiles the javascript template into plain javascript.
-   * 
+   *
    * @param content
    *          the template to be compiled.
    * @param optionalArgument
@@ -43,20 +43,22 @@ public abstract class AbstractJsTemplateCompiler {
   protected abstract String getCompileCommand();
 
   /**
-   * @return additional arguments for the compiler. 
+   * @return additional arguments for the compiler.
    */
   protected String getArguments() {
     return null;
   }
 
-  private String createArgStr(String argument) {
+  private String createArgStr(final String argument) {
     return StringUtils.isNotBlank(argument) ? ", " + argument : "";
   }
-  
+
   /**
    * @return the stream of the compiler resource (javascript) used to compile templates.
+   * @throws IOException
+   *           if the stream could not be located.
    */
-  protected abstract InputStream getCompilerAsStream();
+  protected abstract InputStream getCompilerAsStream() throws IOException;
 
   private RhinoScriptBuilder initScriptBuilder() {
     try {
