61d51bb8ed06c39029119ce0c1112e39ff715c85
Alex Objelean <alex.objelean@gmail.com>
2012-11-23 00:40:06 +0200

    update jslint version

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/JsLintProcessor.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/JsLintProcessor.java
@@ -11,8 +11,10 @@ import ro.isdc.wro.model.resource.SupportedResourceType;
 
 
 /**
- * Processor which analyze the js code and warns you about any problems. The processing result won't change no matter
- * if the processed script contains errors or not.
+ * Processor which analyze the js code and warns you about any problems. The processing result won't change no matter if
+ * the processed script contains errors or not.
+ * <p/>
+ * The version of jshint used by this processor is a snapshot committed on the following date: 2012-11-19 07:22:27.
  *
  * @author Alex Objelean
  * @since 1.4.2
