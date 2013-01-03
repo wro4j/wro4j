a5d3e6d68766bea7faf888ce2e94c46ae3cc5478
Alex Objelean <alex.objelean@gmail.com>
2013-01-03 13:46:54 +0200

    strip jsessionID properly

--- wro4j-core/src/test/java/ro/isdc/wro/model/group/TestDefaultGroupExtractor.java
+++ wro4j-core/src/test/java/ro/isdc/wro/model/group/TestDefaultGroupExtractor.java
@@ -120,7 +120,7 @@ public class TestDefaultGroupExtractor {
 
   @Test
   public void shouldStripJsessionIDFromUrl() {
-    final HttpServletRequest request = mockRequestForUri("https://www.servername.com:80/js/all.js;jsessionID=A327EBE59831FF690C26B0B895EA877E.EFKDD");
+    final HttpServletRequest request = mockRequestForUri("https://www.servername.com:80/js/all.js;jsessionID=A327EBE59831FF690C26B0B895EA877EEFKDD&param.with.dot=value");
     assertEquals("all", groupExtractor.getGroupName(request));
     assertEquals(ResourceType.JS, groupExtractor.getResourceType(request));
   }
