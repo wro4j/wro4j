658577d877afae82a4c11bf68622babb3deddffa
Alex Objelean <alex.objelean@gmail.com>
2013-01-24 00:08:10 +0200

    create WebjarUriLocator

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/locator/WebjarUriLocator.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/locator/WebjarUriLocator.java
@@ -1,24 +1,71 @@
 package ro.isdc.wro.extensions.locator;
 
+import static java.lang.String.format;
+import static org.apache.commons.lang3.Validate.notNull;
+
 import java.io.IOException;
 import java.io.InputStream;
 
-import ro.isdc.wro.model.resource.locator.UriLocator;
+import org.webjars.AssetLocator;
 
+import ro.isdc.wro.model.resource.locator.ClasspathUriLocator;
+import ro.isdc.wro.model.resource.locator.UriLocator;
+import ro.isdc.wro.model.resource.locator.support.LocatorProvider;
+
+/**
+ * Locator responsible for locating webjar resources. A webjar resource is a classpath resource respecting a certain
+ * standard. <a href="http://www.webjars.org/">Read more</a> about webjars.
+ * <p/>
+ * This locator uses the following prefix to identify a locator capable of handling webjar resources:
+ * <code>webjar:</code>
+ *
+ * @author Alex Objelean
+ * @created 6 Jan 2013
+ * @since 1.6.2
+ */
 public class WebjarUriLocator
     implements UriLocator {
-  
+  /**
+   * Alias used to register this locator with {@link LocatorProvider}.
+   */
+  public static final String ALIAS = "webjar";
+  /**
+   * Prefix of the resource uri used to check if the resource can be read by this {@link UriLocator} implementation.
+   */
+  public static final String PREFIX = format("%s:", ALIAS);
+  private final UriLocator classpathLocator = new ClasspathUriLocator();
+
+  /**
+   * @return the uri which is acceptable by this locator.
+   */
+  public static String createUri(final String path) {
+    notNull(path);
+    return PREFIX + path;
+  }
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public InputStream locate(String uri)
+  public InputStream locate(final String uri)
       throws IOException {
-    // TODO Auto-generated method stub
-    return null;
+    try {
+      final String fullpath = AssetLocator.getFullPath(extractPath(uri));
+      return classpathLocator.locate(ClasspathUriLocator.createUri(fullpath));
+    } catch (final Exception e) {
+      throw new IOException("No webjar with uri: " + uri + " available.", e);
+    }
   }
-  
+
+
+  private String extractPath(final String uri) {
+    return uri.replace(PREFIX, "");
+  }
+
+  /**
+   * {@inheritDoc}
+   */
   @Override
-  public boolean accept(String uri) {
-    // TODO Auto-generated method stub
-    return false;
+  public boolean accept(final String uri) {
+    return uri.trim().startsWith(PREFIX);
   }
-  
 }
