8288d68dcac7a52dc66eafa8b115f2833e77d886
Alex Objelean <alex.objelean@gmail.com>
2013-01-23 23:33:21 +0200

    add webjars-locator dependency

---
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/locator/WebjarUriLocator.java
@@ -0,0 +1,24 @@
+package ro.isdc.wro.extensions.locator;
+
+import java.io.IOException;
+import java.io.InputStream;
+
+import ro.isdc.wro.model.resource.locator.UriLocator;
+
+public class WebjarUriLocator
+    implements UriLocator {
+  
+  @Override
+  public InputStream locate(String uri)
+      throws IOException {
+    // TODO Auto-generated method stub
+    return null;
+  }
+  
+  @Override
+  public boolean accept(String uri) {
+    // TODO Auto-generated method stub
+    return false;
+  }
+  
+}
