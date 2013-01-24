4cbc7fa9ada97ef725198e376cfb32ba5a20104c
Alex Objelean <alex.objelean@gmail.com>
2013-01-24 17:34:49 +0200

    remove standaloneContextAwareManagerFactory replaced with StandalonContextAware interface

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/manager/standalone/FingerprintAwareStandaloneManagerFactory.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/manager/standalone/FingerprintAwareStandaloneManagerFactory.java
@@ -3,13 +3,13 @@
  */
 package ro.isdc.wro.extensions.manager.standalone;
 
-import ro.isdc.wro.manager.factory.standalone.StandaloneContextAware;
+import ro.isdc.wro.model.factory.WroModelFactory;
 import ro.isdc.wro.model.resource.support.naming.HashEncoderNamingStrategy;
 
 
 /**
- * An implementation of {@link StandaloneContextAware} which uses a
- * {@link HashEncoderNamingStrategy} when encoding the result file name.
+ * An implementation of {@link WroModelFactory} which uses {@link HashEncoderNamingStrategy} for encoding the result
+ * file name.
  *
  * @author Alex Objelean
  * @created 15 Aug 2010
