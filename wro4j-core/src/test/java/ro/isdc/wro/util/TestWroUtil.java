4e7b47e35be4b06abf06cfbe58fdd344119f8399
Alex Objelean <alex.objelean@gmail.com>
2013-02-08 01:15:15 +0200

    fix failing test -> always use US locale

--- wro4j-core/src/test/java/ro/isdc/wro/util/TestWroUtil.java
+++ wro4j-core/src/test/java/ro/isdc/wro/util/TestWroUtil.java
@@ -161,7 +161,7 @@ public class TestWroUtil {
   }
 
   @Test
-  public void shouldGenerateDateAsString() {
+  public void shouldGenerateDateAsStringWithUSLocale() {
     Locale.setDefault(Locale.ITALY);
     final long milliseconds = 1360278459808l;
     assertEquals("Thu, 07 Feb 2013 23:07:39 GMT", WroUtil.toDateAsString(milliseconds));
