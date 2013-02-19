1daba0d3dbe567061765c01cd2f53ae62f78dc79
Alex Objelean <alex.objelean@gmail.com>
2013-02-20 00:03:02 +0200

    add a class which will encapsulate cssImport pattern usage

---
+++ wro4j-core/src/main/java/ro/isdc/wro/model/resource/processor/support/CssImportInspector.java
@@ -0,0 +1,12 @@
+package ro.isdc.wro.model.resource.processor.support;
+
+/**
+ * Encapsulates the matcher creation for css import statement detection.
+ *
+ * @author Alex Objelean
+ * @created 19 Feb 2013
+ * @since 1.6.3
+ */
+public class CssImportInspector {
+
+}
