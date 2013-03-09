35f92546bd5594f9305a83fc1239c49c12c07ffa
Alex Objelean <alex.objelean@gmail.com>
2013-03-10 01:45:52 +0200

    compute foundErrors & totalProcessedResources to log them after jshint plugin completes

--- wro4j-maven-plugin/src/main/java/ro/isdc/wro/maven/plugin/JsHintMojo.java
+++ wro4j-maven-plugin/src/main/java/ro/isdc/wro/maven/plugin/JsHintMojo.java
@@ -47,6 +47,9 @@ public class JsHintMojo
    */
   private String reportFormat = FormatterType.JSLINT.getFormat();
 
+  private int processedResources = 0;
+  private int foundErrors = 0;
+
   /**
    * {@inheritDoc}
    */
@@ -56,6 +59,7 @@ public class JsHintMojo
       @Override
       public void process(final Resource resource, final Reader reader, final Writer writer)
           throws IOException {
+        processedResources++;
         getLog().info("processing resource: " + resource);
         // use StringWriter to discard the merged processed result (linting is useful only for reporting errors).
         super.process(resource, reader, new StringWriter());
@@ -70,6 +74,7 @@ public class JsHintMojo
       protected void onLinterException(final LinterException e, final Resource resource) {
         final String errorMessage = String.format("%s errors found while processing resource: %s. Errors are: %s", e
             .getErrors().size(), resource, e.getErrors());
+        foundErrors += e.getErrors().size();
         getLog().error(errorMessage);
         // collect found errors
         addReport(ResourceLintReport.create(resource.getUri(), e.getErrors()));
@@ -79,6 +84,13 @@ public class JsHintMojo
       };
     }.setOptionsAsString(getOptions());
     return processor;
+  }
+
+  @Override
+  protected void onAfterExecute() {
+    getLog().info("ONAFTEREXECUTE");
+    getLog().info(String.format("JSHINT found %s errors in %s files", foundErrors, processedResources));
+    super.onAfterExecute();
   }
 
   /**
