b4d1d9e91df9874bd8fa3f32a0db3e7087a06e3a
Alex Objelean <alex.objelean@gmail.com>
2013-04-01 22:42:44 +0300

    add missing imports

--- wro4j-core/src/test/java/ro/isdc/wro/http/handler/TestResourceProxyRequestHandler.java
+++ wro4j-core/src/test/java/ro/isdc/wro/http/handler/TestResourceProxyRequestHandler.java
@@ -1,7 +1,8 @@
 package ro.isdc.wro.http.handler;
 
-import static org.hamcrest.core.Is.is;
-import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.mockito.Matchers.anyString;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
