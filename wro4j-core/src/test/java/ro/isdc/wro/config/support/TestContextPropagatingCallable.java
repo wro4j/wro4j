b35e17ad2c36b697ad08d1c987144e7368ee7e64
Alex Objelean <alex.objelean@gmail.com>
2013-04-17 10:23:47 +0300

    add new test resources

--- wro4j-core/src/test/java/ro/isdc/wro/config/support/TestContextPropagatingCallable.java
+++ wro4j-core/src/test/java/ro/isdc/wro/config/support/TestContextPropagatingCallable.java
@@ -51,6 +51,7 @@ public class TestContextPropagatingCallable {
 
   @Test(expected = WroRuntimeException.class)
   public void shouldFailWhenNoContextIsAvailable() {
+    Context.unset();
     new ContextPropagatingCallable<Void>(NO_OP_CALLABLE);
   }
 
