b9332536b01ee81f767d5c73bd254c61a045dbde
Alex Objelean <alex.objelean@gmail.com>
2013-04-26 00:38:08 +0300

    fix failing unit test

--- wro4j-maven-plugin/src/test/java/ro/isdc/wro/maven/plugin/TestJsHintMojo.java
+++ wro4j-maven-plugin/src/test/java/ro/isdc/wro/maven/plugin/TestJsHintMojo.java
@@ -51,7 +51,7 @@ public class TestJsHintMojo
   @Test
   public void shouldUseMixedOptions()
       throws Exception {
-    getMojo().setOptions("devel,noarg,globalstrict,sub,browser,predef=[\"angular\",\"skf\"]");
+    getMojo().setOptions("devel,noarg,sub,browser,predef=[\"document\",\"jQuery\"]");
     getMojo().setTargetGroups("undef");
     getMojo().execute();
   }
