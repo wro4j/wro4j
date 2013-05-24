e21ac7d69c42ca70e596470d8f9317d9cd6b0259
Alex Objelean <alex.objelean@gmail.com>
2013-05-24 17:26:49 +0300

    load ember.js from webjar and load required dependencies from webjar. Still facing an issue regarding how to invoke the compilation

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/emberjs/EmberJs.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/emberjs/EmberJs.java
@@ -1,19 +1,22 @@
 package ro.isdc.wro.extensions.processor.support.emberjs;
 
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.SequenceInputStream;
+import java.util.Vector;
 
+import ro.isdc.wro.extensions.locator.WebjarUriLocator;
 import ro.isdc.wro.extensions.processor.support.template.AbstractJsTemplateCompiler;
 
 
 /**
  * EmberJS is a framework which provide a templating engine built on top of Handlebars.
- * 
+ *
  * @author blemoine
  */
 public class EmberJs
     extends AbstractJsTemplateCompiler {
-  
+
   // The file ember-template-compiler.js is distributed with Ember JS;
   private static final String DEFAULT_EMBER_TEMPLATE_COMPILER_JS = "ember-template-compiler-1.0.0.rc.1.js";
   /*
@@ -21,7 +24,7 @@ public class EmberJs
    * There is no 'exports' object in Rhino, so this file creates it, as well as an helper function
    */
   private static final String DEFAULT_HEADLESS_RHINO_JS = "headless-rhino.js";
-  
+
   /**
    * visible for testing, the init of a HandlebarsJs template
    */
@@ -30,17 +33,23 @@ public class EmberJs
     return "(function() {Ember.TEMPLATES[" + name + "] = Ember.Handlebars.template(" + super.compile(content, "")
         + ")})();";
   }
-  
+
   @Override
   protected String getCompileCommand() {
     // Function present in headless-ember
     return "precompile";
   }
-  
+
   @Override
-  protected InputStream getCompilerAsStream() {
-    final InputStream headlessRhino = EmberJs.class.getResourceAsStream(DEFAULT_HEADLESS_RHINO_JS);
-    final InputStream emberTemplateCompiler = EmberJs.class.getResourceAsStream(DEFAULT_EMBER_TEMPLATE_COMPILER_JS);
-    return new SequenceInputStream(headlessRhino, emberTemplateCompiler);
+  protected InputStream getCompilerAsStream() throws IOException {
+    final Vector<InputStream> inputStreams = new Vector<InputStream>();
+    inputStreams.add(new WebjarUriLocator().locate(WebjarUriLocator.createUri("jquery.js")));
+    inputStreams.add(new WebjarUriLocator().locate(WebjarUriLocator.createUri("handlebars.js")));
+    inputStreams.add(new WebjarUriLocator().locate(WebjarUriLocator.createUri("ember.js")));
+    inputStreams.add(EmberJs.class.getResourceAsStream(DEFAULT_HEADLESS_RHINO_JS));
+    //inputStreams.add(EmberJs.class.getResourceAsStream(DEFAULT_EMBER_TEMPLATE_COMPILER_JS));
+    //inputStreams.add(fis3);
+
+    return new SequenceInputStream(inputStreams.elements());
   }
 }
--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/emberjs/headless-rhino.js
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/emberjs/headless-rhino.js
@@ -2,7 +2,9 @@ exports = {
     precompile: function () {
     } // will be replaced by ember-template-compiler
 };
-
 precompile = function (arg) {
+	var result = Ember.Handlebars.precompile(arg);
+	print(result)
+	print(typeof result)
     return exports.precompile(arg).toString();
 };
