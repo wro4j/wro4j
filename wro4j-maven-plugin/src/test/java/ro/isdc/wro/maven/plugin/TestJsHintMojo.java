c0ec4f6d7ce1130e92f0b67a5f74c41cae201645
Alex Objelean <alex.objelean@gmail.com>
2013-07-24 18:43:11 +0300

    Add the fix and a test case to prove the fix.

--- wro4j-maven-plugin/src/test/java/ro/isdc/wro/maven/plugin/TestJsHintMojo.java
+++ wro4j-maven-plugin/src/test/java/ro/isdc/wro/maven/plugin/TestJsHintMojo.java
@@ -142,6 +142,20 @@ public class TestJsHintMojo
     executeResourcesWithErrors();
   }
 
+  /**
+   * Checks that build doesn't fail when the failFast is true and there is no resources to be processed.
+   */
+  @Test
+  public void shouldNotFailWhenNoErrorsFound()
+      throws Exception {
+    final JsHintMojo jsHintMojo = (JsHintMojo) getMojo();
+    jsHintMojo.setFailThreshold(0);
+    jsHintMojo.setFailFast(false);
+    jsHintMojo.setIgnoreMissingResources(true);
+    jsHintMojo.setTargetGroups("invalidWildcardResource");
+    getMojo().execute();
+  }
+
   @Test(expected = MojoExecutionException.class)
   public void shouldReportOnlyFirstErrorWhenFailFastIsTrue()
       throws Exception {
