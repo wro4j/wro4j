f50b4d6ea4555c28375268d6e1e83b58731211cc
Alex Objelean <alex.objelean@gmail.com>
2013-11-01 12:30:18 +0200

    remove unnecessary repository in root pom.xml

--- wro4j-extensions/src/test/java/ro/isdc/wro/extensions/processor/TestBourbonCssProcessor.java
+++ wro4j-extensions/src/test/java/ro/isdc/wro/extensions/processor/TestBourbonCssProcessor.java
@@ -20,23 +20,23 @@ import ro.isdc.wro.util.WroTestUtils;
 
 /**
  * Test BourbonCssProcessor.
- * 
+ *
  * @author Simon van der Sluis
  * @created Created on Apr 21, 2010
  */
 public class TestBourbonCssProcessor {
-  
+
   /** Location (base) of bourbon sass css with bourbon test resources. */
   private final URL url = getClass().getResource("bourboncss");
-  
+
   /** A RubySassEngine to test */
   private BourbonCssProcessor processor;
-  
+
   @Before
   public void initEngine() {
     processor = new BourbonCssProcessor();
   }
-  
+
   /**
    * Ignore this test since it doesn't pass consistently because of some internal error of jruby.
    * The error is that the paths used by the sass import directive are based on the current working dir.
@@ -54,10 +54,11 @@ public class TestBourbonCssProcessor {
     final File expectedFolder = new File(url.getFile(), "expected");
     WroTestUtils.compareFromDifferentFoldersByName(testFolder, expectedFolder, "scss", "css", processor);
   }
-  
+
   @Test
   public void shouldBeThreadSafe() throws Exception {
     final Callable<Void> task = new Callable<Void>() {
+      @Override
       public Void call() {
         try {
           processor.process(new StringReader("h3#heading {  font-size: modular-scale(14px, 1, 1.618); }"), new StringWriter());
@@ -70,7 +71,7 @@ public class TestBourbonCssProcessor {
     WroTestUtils.runConcurrently(task);
   }
 
-  
+
   @Test
   public void shouldSupportOnlyCssResources() {
     WroTestUtils.assertProcessorSupportResourceTypes(processor, ResourceType.CSS);
