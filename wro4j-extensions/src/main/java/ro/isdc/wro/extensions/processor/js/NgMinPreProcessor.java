5702aa6db6b97cfc15ca7e873611c166eac6fb60
Janek Lasocki-Biczysko <j.lasocki-biczysko@intrallect.com>
2013-11-11 13:15:55 +0000

    added NgMinPreProcessor

---
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/NgMinPreProcessor.java
@@ -0,0 +1,59 @@
+package ro.isdc.wro.extensions.processor.js;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.io.Reader;
+import java.io.Writer;
+
+import org.apache.commons.exec.CommandLine;
+import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.Executor;
+import org.apache.commons.exec.PumpStreamHandler;
+import org.apache.commons.io.input.ReaderInputStream;
+import org.apache.commons.io.output.WriterOutputStream;
+import org.apache.commons.lang3.StringUtils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import ro.isdc.wro.model.resource.Resource;
+import ro.isdc.wro.model.resource.ResourceType;
+import ro.isdc.wro.model.resource.SupportedResourceType;
+import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
+
+@SupportedResourceType(ResourceType.JS)
+public class NgMinPreProcessor implements ResourcePreProcessor {
+
+	public static final String ALIAS = "ngMin";
+	private static final Logger LOG = LoggerFactory.getLogger(NgMinPreProcessor.class);
+
+	/**
+	 * 'ngmin' must be available on the $PATH, and can be installed via npm:
+	 * 'npm install -g ngmin'
+	 */
+	private static final String NGMIN_COMMAND = "ngmin";
+
+	@Override
+	public void process(final Resource resource, final Reader reader, final Writer writer) throws IOException {
+
+		final OutputStream out = new WriterOutputStream(writer);
+		final InputStream in = new ReaderInputStream(reader);
+
+		final Executor executor = new DefaultExecutor();
+		executor.setStreamHandler(new PumpStreamHandler(out, null, in));
+
+		try {
+
+			executor.execute(CommandLine.parse(NGMIN_COMMAND));
+
+		} catch (IOException ex) {
+
+			final String resourceUri = resource == null ? StringUtils.EMPTY : "[" + resource.getUri() + "]";
+			LOG.warn("Exception while applying " + getClass().getSimpleName() + " processor on the " + resourceUri
+					+ " resource, no processing applied...", ex);
+
+			throw ex;
+		}
+	}
+
+}
