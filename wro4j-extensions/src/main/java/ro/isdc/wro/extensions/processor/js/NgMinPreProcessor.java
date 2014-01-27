d19e924a08f218d07ebcdcd8052f1ec89265afe1
Janek Lasocki-Biczysko <iamjanek@gmail.com>
2014-01-27 23:29:23 +0000

    adding tests for NgMin

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/NgMinPreProcessor.java
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/js/NgMinPreProcessor.java
@@ -1,5 +1,6 @@
 package ro.isdc.wro.extensions.processor.js;
 
+import java.io.ByteArrayInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
@@ -8,6 +9,7 @@ import java.io.Writer;
 
 import org.apache.commons.exec.CommandLine;
 import org.apache.commons.exec.DefaultExecutor;
+import org.apache.commons.exec.ExecuteException;
 import org.apache.commons.exec.Executor;
 import org.apache.commons.exec.PumpStreamHandler;
 import org.apache.commons.io.input.ReaderInputStream;
@@ -20,9 +22,10 @@ import ro.isdc.wro.model.resource.Resource;
 import ro.isdc.wro.model.resource.ResourceType;
 import ro.isdc.wro.model.resource.SupportedResourceType;
 import ro.isdc.wro.model.resource.processor.ResourcePreProcessor;
+import ro.isdc.wro.model.resource.processor.SupportAware;
 
 @SupportedResourceType(ResourceType.JS)
-public class NgMinPreProcessor implements ResourcePreProcessor {
+public class NgMinPreProcessor implements ResourcePreProcessor, SupportAware {
 
 	public static final String ALIAS = "ngMin";
 	private static final Logger LOG = LoggerFactory.getLogger(NgMinPreProcessor.class);
@@ -39,14 +42,11 @@ public class NgMinPreProcessor implements ResourcePreProcessor {
 		final OutputStream out = new WriterOutputStream(writer);
 		final InputStream in = new ReaderInputStream(reader);
 
-		final Executor executor = new DefaultExecutor();
-		executor.setStreamHandler(new PumpStreamHandler(out, null, in));
-
 		try {
 
-			executor.execute(CommandLine.parse(NGMIN_COMMAND));
+			process(in, out);
 
-		} catch (IOException ex) {
+		} catch (final IOException ex) {
 
 			final String resourceUri = resource == null ? StringUtils.EMPTY : "[" + resource.getUri() + "]";
 			LOG.warn("Exception while applying " + getClass().getSimpleName() + " processor on the " + resourceUri
@@ -54,6 +54,32 @@ public class NgMinPreProcessor implements ResourcePreProcessor {
 
 			throw ex;
 		}
+	}
+
+	private int process(final InputStream in, final OutputStream out) throws ExecuteException, IOException {
+		final Executor executor = new DefaultExecutor();
+		executor.setStreamHandler(new PumpStreamHandler(out, null, in));
+		return executor.execute(CommandLine.parse(NGMIN_COMMAND));
+	}
+
+	@Override
+	public boolean isSupported() {
+
+		try {
+			final InputStream input = new ByteArrayInputStream("".getBytes("UTF-8"));
+			final int result = process(input, null);
+
+			if (result == 0) {
+				LOG.debug("The {} processor is supported.", getClass().getName());
+				return true;
+			} else {
+				LOG.debug("The {} processor is not supported, test returned non-0 exit code.");
+			}
+		} catch (final Exception e) {
+			LOG.debug("The {} processor is not supported. Because: {}", getClass().getName(), e.getMessage());
+		}
+
+		return false;
 	}
 
 }
