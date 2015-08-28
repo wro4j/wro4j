7a757b78c15e9dbdfe15070f126eb6fe7f421cb3
Alex Objelean <alex.objelean@gmail.com>
2015-08-29 00:17:15 +0300

    issue #970: use buildContext to refresh the file after each group is processed (required for incremental build). This fix is experimental, it does not contain any tests yet and is not clear if it fixes the original problem.

--- wro4j-maven-plugin/src/main/java/ro/isdc/wro/maven/plugin/Wro4jMojo.java
+++ wro4j-maven-plugin/src/main/java/ro/isdc/wro/maven/plugin/Wro4jMojo.java
@@ -25,6 +25,7 @@ import org.apache.commons.io.output.ByteArrayOutputStream;
 import org.apache.commons.lang3.StringUtils;
 import org.apache.maven.plugin.MojoExecutionException;
 import org.mockito.Mockito;
+import org.sonatype.plexus.build.incremental.BuildContext;
 
 import ro.isdc.wro.config.Context;
 import ro.isdc.wro.config.jmx.WroConfiguration;
@@ -93,6 +94,10 @@ public class Wro4jMojo
    * @optional
    */
   private String contextPath;
+  /** 
+   * @component 
+   */
+  private BuildContext buildContext;
   /**
    * Holds a mapping between original group name file & renamed one.
    */
@@ -300,6 +305,10 @@ public class Wro4jMojo
         getLog().info(destinationFile.getAbsolutePath() + " (" + destinationFile.length() + " bytes" + ")");
       }
     } finally {
+      // instruct the build about the change in context of incremental build
+      if (buildContext != null) {
+        buildContext.refresh(parentFoder);
+      }
       if (resultOutputStream != null) {
         resultOutputStream.close();
       }
