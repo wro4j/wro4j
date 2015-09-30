e41d37d174f8d5866195d96a6d6cfab2421c0f51
Alex Objelean <alex.objelean@gmail.com>
2015-09-30 15:04:46 +0300

    remove unused script

--- wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/less/init.js
+++ wro4j-extensions/src/main/java/ro/isdc/wro/extensions/processor/support/less/init.js
@@ -1,14 +1,4 @@
 /**
- * Creates location object used by less.js 
-*/
-/**
-var location = location || {
-    protocol: "",
-    hostname: "",
-    port: ""
-};
-*/
-/**
  * The problem is that less.js runs in develoment mode and sets a timer (via
  * window.setInterval()). The env.rhino.js implementation of window.setInterval
  * spawns a backround thread (java.lang.Thread(Runnable)) to fire the timer
